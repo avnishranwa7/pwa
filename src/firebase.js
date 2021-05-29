b82db1d53a21e5b9552a20b7270de3da71f5f334
avnish ranwa <avnishranwa@jklu.edu.in>
2021-05-29 14:38:53 +0530

    commit

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,12 @@
 import firebase from 'firebase';
 
 var firebaseConfig = {
-  apiKey: "AIzaSyBN_FLDgL4ZcypvbcPURmMP_Uv-uSKnY9s",
-  authDomain: "pwa-ad782.firebaseapp.com",
-  projectId: "pwa-ad782",
-  storageBucket: "pwa-ad782.appspot.com",
-  messagingSenderId: "990663954593",
-  appId: "1:990663954593:web:e5f14b81c9d7f3f5c4e0e0"
+  apiKey: "AIzaSyDURAcGKhIh7-zXwsdxnCuBixBZzkbJRYE",
+  authDomain: "new-pwa-aecd7.firebaseapp.com",
+  projectId: "new-pwa-aecd7",
+  storageBucket: "new-pwa-aecd7.appspot.com",
+  messagingSenderId: "286101494322",
+  appId: "1:286101494322:web:3e402f3c1c61d88d597a2b"
 };
   // Initialize Firebase
 const fire = firebase.initializeApp(firebaseConfig);
